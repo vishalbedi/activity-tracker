cbd43e56ebb079b0604e8edcd64ada324fd2156e
Vishal <vishal.b1989@gmail.com>
2018-09-16 19:26:59 -0400

    added add day form

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropType from 'prop-types'
 
-export const AddDayForm = () =>
-    <div>
-        <h1>
-            Add a Day!
-        </h1>
-    </div>;
+export class AddDayForm extends Component{
+    render() {
+        return (
+            <form className="add-day">
+                <label htmlFor="resort">Resort Name</label>
+                <input type="text" id="resort" required/>
+
+                <label htmlFor="date">Date</label>
+                <input type="date" id="date" required/>
+
+                <div>
+                    <input type="checkbox" id="powder" />
+                    <label htmlFor="powder">Powder</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="backcountry"/>
+                    <label htmlFor="backcountrys">Backcountry</label>
+                </div>
+
+            </form>
+        );
+    }
+}
+AddDayForm.propTypes = {
+    resort: PropType.string.isRequired,
+    date : PropType.string.isRequired,
+    powder : PropType.bool.isRequired,
+    backcountry: PropType.bool.isRequired
+};
